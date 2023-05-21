d6d8c5c2f5cb8990580414f5236a387816272243
r-s-coding <112776326+r-s-coding@users.noreply.github.com>
2023-05-21 18:35:06 +0000

    Added Component Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import Favourites from './components/Favourites';
+import Meals from './components/Meals';
+import Modal from './components/Modal';
+import Search from './components/Search';
 
 export default function App() {
   return (
     <main>
-      Meals App
+      <Favourites />
+      <Meals />
+      <Modal />
+      <Search />
     </main>
   )
 }
