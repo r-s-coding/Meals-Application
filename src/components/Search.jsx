97d2f2c7f658da842352b0d9dfff1f1c8843d072
r-s-coding <112776326+r-s-coding@users.noreply.github.com>
2023-05-23 01:23:27 +0000

    Added search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,36 @@
+import {useState} from 'react'
+import {useGlobalContext} from '../context'
+
 const Search = () => {
+  const [text, setText] = useState('')
+
+  const {setSearchTerm, fetchRandomMeal} = useGlobalContext()
+  
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text){
+      setSearchTerm(text)
+    }
+  }
+
+  const handleRandomMeal = () => {
+    setSearchTerm('')
+    setText('')
+    fetchRandomMeal()
+  }
+  
   return (
-    <h1>Search Component</h1>
+    <header className="search-container">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Type Favourite Meal" value={text} onChange={handleChange} className="form-input"/>
+        <button type="submit" className="btn">Search</button>
+        <button type="button" className="btn btn-hipster" onClick={handleRandomMeal}>Surprise Me!</button>
+      </form>
+    </header>
   )
 }
 
--- src/context.jsx
+++ src/context.jsx
@@ -7,8 +7,10 @@ const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const AppProvider = ({children}) => {
+  
   const [meals, setMeals] = useState([])
   const [loading, setLoading] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
   
   const fetchMeals = async (url) => {
     setLoading(true)
@@ -24,14 +26,23 @@ const AppProvider = ({children}) => {
     }
     setLoading(false)
   }
+
+  const fetchRandomMeal = () => {
+    fetchMeals(randomMealUrl)
+  }
   
   // Can only use async/await by either creating a function inside or outside the useEffect function
   useEffect(() => {
     fetchMeals(allMealsUrl)
   }, [])
   
+  useEffect(() => {
+    if (!searchTerm) return
+    fetchMeals(`${allMealsUrl}${searchTerm}`)
+  }, [searchTerm])
+  
   return (
-    <AppContext.Provider value={{loading, meals}}>
+    <AppContext.Provider value={{loading, meals, setSearchTerm, fetchRandomMeal}}>
       {children}    
     </AppContext.Provider>
   )
